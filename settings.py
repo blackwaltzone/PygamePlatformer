86ab26f4a5f7f15f3f51c45170cb523eb9400dc6
blackwaltzone <dustin.heyden@gmail.com>
2022-02-01 09:06:37 -0600

    Add files via upload
    completed tutorial

--- settings.py
+++ settings.py
@@ -1,18 +1,12 @@
-level_map = [
-    '                   XXX',
-    '  X   X   X   X      X',
-    '                     X',
-    'XXX                XXX',
-    '  XX        XX        ',
-    '       X    XXXXX     ',
-    '      XX   XXX   X    ',
-    'XP                   X',
-    'XXX                XXX',
-    'XXXXXXXX  XXXX    XXXX'
-]
-
 vertical_tile_number = 11
 tile_size = 64
-screen_width = 1400
+screen_width = 1280
 #screen_height = len(level_map) * tile_size
 screen_height = vertical_tile_number * tile_size
+
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+WHITE = (250, 250, 250)
+GRAY = (100, 100, 100)
+BLUE = (0, 0, 250)
+GREEN = (0, 250, 0)
--- support.py
+++ support.py
@@ -1,10 +1,12 @@
 from os import walk
 import pygame
+from csv import reader
+from settings import tile_size
 
 def import_folder(path):
     surface_list = []
 
-    for _dir_name, _2, img_files in walk(path):
+    for _dir_name, _sub_folders, img_files in walk(path):
         for image in img_files:
             full_path = path + '/' + image
             # make sure only image files are in folder
@@ -12,3 +14,27 @@ def import_folder(path):
             surface_list.append(img_surface)
 
     return surface_list
+
+def import_csv_layout(path):
+    terrain_map = []
+    with open(path) as map:
+        level = reader(map, delimiter = ',')
+        for row in level:
+            terrain_map.append(list(row))
+    return terrain_map
+
+def import_cut_graphic(path):
+    surface = pygame.image.load(path).convert_alpha()
+    tile_num_x = int(surface.get_size()[0] / tile_size)
+    tile_num_y = int(surface.get_size()[1] / tile_size)
+
+    cut_tiles = []
+    for row in range(tile_num_y):
+        for col in range(tile_num_x):
+            x = col * tile_size
+            y = row * tile_size
+            new_surf = pygame.Surface((tile_size, tile_size), flags = pygame.SRCALPHA)
+            new_surf.blit(surface, (0, 0), pygame.Rect(x, y, tile_size, tile_size))
+            cut_tiles.append(new_surf)
+            
+    return cut_tiles
